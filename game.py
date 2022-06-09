e7fb634e6af4b7d46b0603817ff2bfe9633421b1
MarioCazarez9 <mariosony1@gmail.com>
2022-06-09 13:54:14 -0600

    comndo de imprimir tiempo

--- game.py
+++ game.py
@@ -44,6 +44,9 @@ while True:
 for hora in range(24):
     for minuto in range(60):
         for segundo in range(60):
+            os.sytem('cls')
+            print(f'{hora}:{minuto}:{segundo}')
+            time.sleep(1) 
 
 
 
